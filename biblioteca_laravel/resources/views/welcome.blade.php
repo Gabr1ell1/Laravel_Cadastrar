671e42a81ec56087482be919b624f1f4be69aa60
Gabr1ell1 <gabriellynasser16@gmail.com>
2025-04-09 20:33:07 -0300

    Laravel

---
+++ biblioteca_laravel/resources/views/welcome.blade.php
@@ -0,0 +1,65 @@
+<!DOCTYPE html> 
+<html lang="pt"> 
+<head> 
+  <meta charset="utf-8"> 
+  <meta name="viewport" content="width=device-width, initial-scale=1"> 
+  <title>Cadastro de Produtos</title> 
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+  <style>
+    body{font-family: Arial, Helvetica, sans-serif;}
+    form{margin-left: 20px;margin-right: 30px;}
+    .text{margin-left: 20px;}
+  </style>
+</head> 
+<body> 
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#">Dev. Web</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDarkDropdown" aria-controls="navbarNavDarkDropdown" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNavDarkDropdown">
+      <ul class="navbar-nav">
+        <li class="nav-item dropdown">
+          <button class="btn btn-dark dropdown-toggle" data-bs-toggle="dropdown" aria-expanded="false">
+            CRUD
+          </button>
+          <ul class="dropdown-menu dropdown-menu-dark">
+          <li><a class="dropdown-item" href="{{ route('cadastrar-produto')}}">Cadastrar</a></li>
+            <li><a class="dropdown-item" href="{{ route('listar-produto')}}">Listar</a></li>
+            <li><a class="dropdown-item" href="{{ route('editar-produto', ['id' => 2])}}">Atualizar</a></li>
+            <li><a class="dropdown-item" href="#">Deletar</a></li>
+          </ul>
+        </li>
+      </ul>
+    </div>
+  </div>
+</nav>
+
+  <div class="text">
+  <p><h4>Cadastre Novos Produtos!</h4>
+    Sistema de cadastro de produtos 
+  </p> 
+  </div>
+  <form action="/cadastrar-produto" method="POST"> 
+    @csrf 
+    <div class="form-floating">
+      <input type="text" class="form-control" name="nome"> 
+      <label for="lblNome">Nome: </label> 
+    </div><br>
+    <div class="form-floating mb-3">
+      <input type="text" class="form-control" name="valor"> 
+      <label for="lblValor">Valor: </label> 
+    </div>
+    <div class="form-floating mb-3">
+      <input type="text" class="form-control" name="quantidade"> 
+      <label for="lblQuantidade">Quantidade: </label> 
+    </div>
+    <br> 
+    <button  class="btn btn-primary" >Cadastrar</button> 
+  </form> 
+
+
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+</body> 
+</html>
---
+++ biblioteca_laravel/routes/web.php
@@ -0,0 +1,70 @@
+<?php 
+use Illuminate\Support\Facades\Route; 
+
+/*Web Routes 
+Here is where you can register web routes for your application. These routes are loaded by the RouteServiceProvider within a group which contains the "web" middleware group. Now create something great! 
+*/
+use App\Models\Produto; 
+use Illuminate\Http\Request; 
+
+
+Route::get('/', function () { 
+    return view('welcome'); 
+}) ->name('cadastrar-produto'); 
+
+Route::post('/cadastrar-produto', function (Request $request) { 
+    //dd($request->all()); 
+    Produto::create([ 
+    'nome' => $request->nome, 
+    'valor' => $request->valor, 
+    'quantidade' => $request->quantidade 
+]); 
+return redirect('/')->with('sucess', 'Produto cadastrado com sucesso!');
+
+});
+
+Route::get('/listar-produto/{id}', function ($id) { 
+    //dd(Produto::find($id));
+    
+    $produto = Produto::find($id);
+    return view('listar', ['produto' => $produto]);
+
+}) ->name('listar-produto');
+
+Route::get('/editar-produto/{id}', function ($id) { 
+    //dd(Produto::find($id));
+    
+    $produto = Produto::find($id);
+    return view('editar', ['produto' => $produto]);
+
+})->name('editar-produto');
+
+Route::post('/editar-produto/{id}', function (Request $request, $id) 
+{ 
+    //dd(Produto::find($id));    
+    $produto = Produto::find($id);
+
+    $produto->update([
+        'nome' => $request->nome,
+        'valor' => $request->valor,
+        'quantidade' => $request->quantidade
+    ]);
+    return redirect('/')->with('sucess', 'Produto editado com sucesso!');
+
+});
+
+Route::get('/excluir-produto/{id}', function ($id) { 
+    //dd(Produto::find($id));
+    
+    $produto = Produto::find($id);
+    $produto->delete();
+
+    if($produto){
+        $produto->delete();
+        return redirect('/')->with('sucess', 'Produto excluido com sucesso!');
+    }else{
+        return redirect('/')->with('error', 'Produto não encontrado');
+    }
+})->name('excluir-produto');
+
+
